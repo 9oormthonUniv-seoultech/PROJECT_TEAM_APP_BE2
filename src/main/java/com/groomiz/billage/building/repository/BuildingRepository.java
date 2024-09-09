e5bf3d4fdbae1f85af28c2e44ad258190ccd4fde
chaeyoungeee <chaeinge@gmail.com>
2024-09-10 02:17:42 +0900

    SCRUM-12 Feat: repository 추가

---
+++ src/main/java/com/groomiz/billage/building/repository/BuildingRepository.java
@@ -0,0 +1,8 @@
+package com.groomiz.billage.building.repository;
+
+import com.groomiz.billage.building.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+
+}
