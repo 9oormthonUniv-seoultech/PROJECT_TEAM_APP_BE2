4077e5d72c6e8fd4dbbb78e54c358b641d65a29c
hawardShin <joushin@student.42seoul.kr>
2024-09-13 16:14:27 +0900

    Rename: RedisConfig move  to global

--- src/main/java/com/groomiz/billage/global/config/RedisConfig.java
+++ src/main/java/com/groomiz/billage/global/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.groomiz.billage.auth.config;
+package com.groomiz.billage.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
