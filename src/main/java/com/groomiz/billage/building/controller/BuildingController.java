7ab84ffc3f011c1fe7995357218c7c2f8e72aeba
chaeyoungeee <chaeinge@gmail.com>
2024-09-24 14:42:47 +0900

    SCRUM-18 Feat: 건물 관련 요청/응답 생성

---
+++ src/main/java/com/groomiz/billage/building/controller/BuildingController.java
@@ -0,0 +1,35 @@
+package com.groomiz.billage.building.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.groomiz.billage.building.dto.response.BuildingListResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/univ/building")
+@Tag(name = "Building Controller", description = "[학생] 건물 관련 API")
+public class BuildingController {
+
+	@GetMapping
+	@Operation(summary = "건물 목록 조회")
+	public List<BuildingListResponse> findAll(
+		@Parameter(description = "예약 날짜", example = "2024-07-01")
+		@RequestParam("date") LocalDate date,
+		@Parameter(description = "예약 인원", example = "30")
+		@RequestParam("headcount") Integer count) {
+
+		List<BuildingListResponse> response = null;
+		return response;
+	}
+}
