3c610eeabc1887ee90996f462081b697dd29deb7
changha <mentenseoul@gmail.com>
2024-09-26 12:51:59 +0900

    SCRUM-19 Refactor: 전화번호 수정 API
    전화번호 수정 API -> 회원 정보 API로 수정(이메일 추가)

--- src/main/java/com/groomiz/billage/member/dto/MemberInfoRequest.java
+++ src/main/java/com/groomiz/billage/member/dto/MemberInfoRequest.java
@@ -4,9 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-@Schema(description = "전화번호 수정 요청 DTO")
-public class PhoneNumberRequest {
+@Schema(description = "회원 정보 수정 요청 DTO")
+public class MemberInfoRequest {
 
 	@Schema(description = "전화번호", example = "010-1234-5678")
 	private String phoneNumber;
+
+	@Schema(description = "이메일", example = "asdf1234@gmail.com")
+	private String email;
+
 }
