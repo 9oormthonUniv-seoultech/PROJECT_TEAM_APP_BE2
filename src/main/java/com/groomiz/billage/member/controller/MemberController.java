72b71ff44b5866db4f128e5d64860befa4ff5216
changha <mentenseoul@gmail.com>
2024-09-26 12:51:59 +0900

    SCRUM-19 Refactor: 전화번호 수정 API
    전화번호 수정 API -> 회원 정보 API로 수정(이메일 추가)

--- src/main/java/com/groomiz/billage/member/controller/MemberController.java
+++ src/main/java/com/groomiz/billage/member/controller/MemberController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.groomiz.billage.member.dto.MemberInfoRequest;
 import com.groomiz.billage.member.dto.MemberInfoResponse;
 import com.groomiz.billage.member.dto.PasswordRequest;
-import com.groomiz.billage.member.dto.PhoneNumberRequest;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -31,8 +31,8 @@ public class MemberController {
 	}
 
 	@PutMapping("/phone")
-	@Operation(summary = "전화번호 수정")
-	public String updatePhoneNumber(@RequestBody PhoneNumberRequest phoneNumberRequest) {
+	@Operation(summary = "회원 정보 수정")
+	public String updatePhoneNumber(@RequestBody MemberInfoRequest memberInfoRequest) {
 		return "success";
 	}
 
