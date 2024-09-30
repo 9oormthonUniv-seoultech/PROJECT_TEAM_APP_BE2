9b231c0704b5aada5d34fb0b0be9752af91dbda1
hawardShin <joushin@student.42seoul.kr>
2024-09-30 12:11:44 +0900

    Feat: set domain exception example

--- src/main/java/com/groomiz/billage/global/document/ExampleController.java
+++ src/main/java/com/groomiz/billage/global/document/ExampleController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.groomiz.billage.global.dto.ExampleReqDto;
-import com.groomiz.billage.global.dto.ExampleResDto;
+import com.groomiz.billage.global.anotation.ApiErrorCodeExample;
+import com.groomiz.billage.global.exception.GlobalErrorCode;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,11 +17,8 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "Exception Document", description = "예제 에러코드 문서화")
 public class ExampleController {
 	@GetMapping("/global")
+	@ApiErrorCodeExample(GlobalErrorCode.class)
 	@Operation(summary = "글로벌 (aop, 서버 내부 오류등)  관련 에러 코드 나열")
-	public ExampleResDto example(ExampleReqDto exampleReqDto) {
-		ExampleResDto exampleResDto = new ExampleResDto();
-		exampleResDto.setName(exampleReqDto.getName());
-		exampleResDto.setAge(exampleReqDto.getAge());
-		return exampleResDto;
+	public void example() {
 	}
 }
