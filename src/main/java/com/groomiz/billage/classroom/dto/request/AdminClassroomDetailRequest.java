ae7236835d467d7165920e4cb7f9d84d244b9241
orieasy1 <easy1nhard2@gmail.com>
2024-09-30 20:58:48 +0900

    feat: requestDTO에 validation 추가

--- src/main/java/com/groomiz/billage/classroom/dto/request/AdminClassroomDetailRequest.java
+++ src/main/java/com/groomiz/billage/classroom/dto/request/AdminClassroomDetailRequest.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +19,13 @@ import lombok.NoArgsConstructor;
 @Schema(description = "강의실 상세 조회 요청 DTO")
 public class AdminClassroomDetailRequest {
 
+	@NotNull
+	@Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "날짜 형식은 yyyy-MM-dd여야 합니다.")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Schema(description = "조회할 날짜", example = "2024-09-03")
 	private LocalDate date;
 
+	@NotNull
 	@Schema(description = "건물 ID", example = "1")
 	private Long building;
 
--- src/main/java/com/groomiz/billage/classroom/dto/request/AdminClassroomStatusRequest.java
+++ src/main/java/com/groomiz/billage/classroom/dto/request/AdminClassroomStatusRequest.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +19,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "강의실 현황 필터링 요청 DTO")
 public class AdminClassroomStatusRequest {
-	
+
+	@NotNull
+	@Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "날짜 형식은 yyyy-MM-dd여야 합니다.")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@Schema(description = "날짜", example = "2024-09-04")
 	private LocalDate date;
