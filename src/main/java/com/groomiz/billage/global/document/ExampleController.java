f47eccdec0ea95e2ce8a2c64b4c3b059294a04ca
hawardShin <joushin@student.42seoul.kr>
2024-10-01 20:15:30 +0900

    Feat: Set Example Controller

--- src/main/java/com/groomiz/billage/global/document/ExampleController.java
+++ src/main/java/com/groomiz/billage/global/document/ExampleController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.groomiz.billage.auth.exception.AuthErrorCode;
+import com.groomiz.billage.building.exception.BuildingErrorCode;
+import com.groomiz.billage.classroom.exception.ClassroomErrorCode;
 import com.groomiz.billage.global.anotation.ApiErrorCodeExample;
 import com.groomiz.billage.global.exception.GlobalErrorCode;
+import com.groomiz.billage.reservation.exception.ReservationErrorCode;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,4 +25,35 @@ public class ExampleController {
 	@Operation(summary = "글로벌 (aop, 서버 내부 오류등)  관련 에러 코드 나열")
 	public void example() {
 	}
+
+	@GetMapping("/building")
+	@ApiErrorCodeExample(BuildingErrorCode.class)
+	@Operation(summary = "건물 관련 에러 코드 나열")
+	public void example2() {
+	}
+
+	@GetMapping("/auth")
+	@ApiErrorCodeExample(AuthErrorCode.class)
+	@Operation(summary = "인증 관련 에러 코드 나열")
+	public void example3() {
+	}
+
+	@GetMapping("/classroom")
+	@ApiErrorCodeExample(ClassroomErrorCode.class)
+	@Operation(summary = "강의실 관련 에러 코드 나열")
+	public void example4() {
+	}
+
+	@GetMapping("/member")
+	@ApiErrorCodeExample(ClassroomErrorCode.class)
+	@Operation(summary = "회원 관련 에러 코드 나열")
+	public void example5() {
+	}
+
+	@GetMapping("/reservation")
+	@ApiErrorCodeExample(ReservationErrorCode.class)
+	@Operation(summary = "예약 관련 에러 코드 나열")
+	public void example6() {
+	}
+
 }
