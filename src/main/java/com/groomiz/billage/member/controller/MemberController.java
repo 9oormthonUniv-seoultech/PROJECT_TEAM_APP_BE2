30be6c2e45f632aa0023900fe19cbe5df7649dc7
hawardShin <joushin@student.42seoul.kr>
2024-10-01 20:51:23 +0900

    Feat: 컨트롤러별 에러 코드 정의

--- src/main/java/com/groomiz/billage/member/controller/MemberController.java
+++ src/main/java/com/groomiz/billage/member/controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.groomiz.billage.global.anotation.ApiErrorExceptionsExample;
+import com.groomiz.billage.member.document.UserInfoExceptionDocs;
 import com.groomiz.billage.member.dto.MemberInfoRequest;
 import com.groomiz.billage.member.dto.MemberInfoResponse;
 import com.groomiz.billage.member.dto.PasswordRequest;
@@ -24,6 +26,7 @@ public class MemberController {
 
 	@GetMapping("/info")
 	@Operation(summary = "회원 정보 조회")
+	@ApiErrorExceptionsExample(UserInfoExceptionDocs.class)
 	public ResponseEntity<MemberInfoResponse> info() {
 
 		MemberInfoResponse response = null;
