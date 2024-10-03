8c8cb2bfd45fe6b7f5271f4fe299a43b5e99fabd
changha <mentenseoul@gmail.com>
2024-10-03 20:08:13 +0900

    SCRUM-23 Refactor: 에러코드 넘버 수정
    MemberErrorCode 번호 증가 방식으로 변경

--- src/main/java/com/groomiz/billage/member/exception/MemberErrorCode.java
+++ src/main/java/com/groomiz/billage/member/exception/MemberErrorCode.java
@@ -32,25 +32,25 @@ public enum MemberErrorCode implements BaseErrorCode {
 	PASSWORD_CONSTRAINT_VIOLATION(BAD_REQUEST, "MEMBER_400_5", "비밀번호가 요구 조건에 맞지 않습니다."),
 
 	@ExplainError("이메일 형식이 알맞지 않는 경우 발생하는 오류입니다.")
-	INVALID_EMAIL(BAD_REQUEST, "MEMBER_400_5", "이메일 형식이 요구 조건에 맞지 않습니다."),
+	INVALID_EMAIL(BAD_REQUEST, "MEMBER_400_6", "이메일 형식이 요구 조건에 맞지 않습니다."),
 
 	@ExplainError("이미 가입된 이메일인 경우 발생하는 오류입니다.")
-	EMAIL_ALREADY_REGISTERED(CONFLICT, "MEMBER_409_1", "이미 등록된 이메일입니다."),
+	EMAIL_ALREADY_REGISTERED(CONFLICT, "MEMBER_409_2", "이미 등록된 이메일입니다."),
 
 	@ExplainError("사용자가 입력한 인증 코드가 전송된 코드와 일치하지 않는 경우 발생하는 오류입니다.")
-	INVALID_VERIFICATION_CODE(BAD_REQUEST, "MEMBER_400_4", "인증코드가 일치하지 않습니다."),
+	INVALID_VERIFICATION_CODE(BAD_REQUEST, "MEMBER_400_7", "인증코드가 일치하지 않습니다."),
 
 	@ExplainError("인증 코드가 만료된 경우 발생하는 오류입니다.")
-	EXPIRATION_VERIFICATION_CODE(BAD_REQUEST, "MEMBER_400_4", "인증코드가 만료 되었습니다."),
+	EXPIRATION_VERIFICATION_CODE(BAD_REQUEST, "MEMBER_400_8", "인증코드가 만료 되었습니다."),
 
 	@ExplainError("단과대와 학과가 일치하지 않는 경우 발생하는 오류입니다.")
-	INVALID_COLLEGE_AND_MAJOR(BAD_REQUEST, "MEMBER_400_4", "단과대와 학과가 일치하지 않습니다."),
+	INVALID_COLLEGE_AND_MAJOR(BAD_REQUEST, "MEMBER_400_9", "단과대와 학과가 일치하지 않습니다."),
 
 	@ExplainError("단과대 값이 유효하지 않은 enum인 경우 발생하는 오류입니다.")
-	INVALID_COLLEGE_ENUM(BAD_REQUEST, "MEMBER_400_4", "잘못된 단과대 값입니다."),
+	INVALID_COLLEGE_ENUM(BAD_REQUEST, "MEMBER_400_10", "잘못된 단과대 값입니다."),
 
 	@ExplainError("학과 값이 유효하지 않은 enum인 경우 발생하는 오류입니다.")
-	INVALID_MAJOR_ENUM(BAD_REQUEST, "MEMBER_400_4", "잘못된 학과 값입니다."),
+	INVALID_MAJOR_ENUM(BAD_REQUEST, "MEMBER_400_11", "잘못된 학과 값입니다."),
 
 	@ExplainError("회원이 존재하지 않는 경우 발생하는 오류입니다.")
 	MEMBER_NOT_FOUND(NOT_FOUND, "MEMBER_404_1", "해당 회원이 존재하지 않습니다.");
