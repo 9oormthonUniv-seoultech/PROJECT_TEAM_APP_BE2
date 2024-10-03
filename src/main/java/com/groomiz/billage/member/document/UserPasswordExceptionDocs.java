807a6f72d2426778718c855be59a78453c46543e
hawardShin <joushin@student.42seoul.kr>
2024-10-03 20:02:45 +0900

    Fix: Exception Docs Annotation

--- src/main/java/com/groomiz/billage/member/document/UserPasswordExceptionDocs.java
+++ src/main/java/com/groomiz/billage/member/document/UserPasswordExceptionDocs.java
@@ -2,12 +2,14 @@ package com.groomiz.billage.member.document;
 
 import com.groomiz.billage.auth.exception.AuthErrorCode;
 import com.groomiz.billage.auth.exception.AuthException;
+import com.groomiz.billage.global.anotation.ExceptionDoc;
 import com.groomiz.billage.global.anotation.ExplainError;
 import com.groomiz.billage.global.exception.GlobalCodeException;
 import com.groomiz.billage.global.interfaces.SwaggerExampleExceptions;
 import com.groomiz.billage.member.exception.MemberErrorCode;
 import com.groomiz.billage.member.exception.MemberException;
 
+@ExceptionDoc
 public class UserPasswordExceptionDocs implements SwaggerExampleExceptions {
 	@ExplainError
 	public GlobalCodeException 토큰_만료 = new AuthException(AuthErrorCode.TOKEN_EXPIRED);
