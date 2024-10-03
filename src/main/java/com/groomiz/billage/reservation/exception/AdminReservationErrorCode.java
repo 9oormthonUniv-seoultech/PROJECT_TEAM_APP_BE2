dbd50ee7692fc6344095ca7ca20a265cca024ba0
orieasy1 <easy1nhard2@gmail.com>
2024-10-03 22:46:04 +0900

    Refactor: AdminReservationErrorCode 에러 및  수정

--- src/main/java/com/groomiz/billage/reservation/exception/AdminReservationErrorCode.java
+++ src/main/java/com/groomiz/billage/reservation/exception/AdminReservationErrorCode.java
@@ -14,13 +14,16 @@ import lombok.Getter;
 public enum AdminReservationErrorCode implements BaseErrorCode {
 
 	@ExplainError("예약 타입이 유효하지 않을 때 발생하는 오류입니다.")
-	INVALID_STATUS_FORMAT(BAD_REQUEST, "RESERVATION_400_1", "예약 상태 값이 올바르지 않습니다. 허용되는 값은 'pending', 'approve', 'reject'입니다."),
+	INVALID_STATUS_FORMAT(BAD_REQUEST, "ADMIN_RESERVATION_400_1", "예약 상태 값이 올바르지 않습니다. 허용되는 값은 'pending', 'approve', 'reject'입니다."),
 
 	@ExplainError("예약 타입 파라미터가 누락되었을 때 발생하는 오류입니다.")
-	MISSING_STATUS_PARAMETER(BAD_REQUEST, "RESERVATION_400_2", "예약 상태 파라미터가 필요합니다."),
+	MISSING_STATUS_PARAMETER(BAD_REQUEST, "ADMIN_RESERVATION_400_2", "예약 상태 파라미터가 필요합니다."),
 
 	@ExplainError("예약 유형 값이 올바르지 않을 때 발생하는 오류입니다.")
-	INVALID_TYPE(BAD_REQUEST, "RESERVATION_400_8", "예약 유형 값이 올바르지 않습니다. 허용되는 값은 'single', 'period', 'recurring'입니다.");
+	INVALID_RESERVATION_TYPE(BAD_REQUEST, "ADMIN_RESERVATION_400_3", "예약 유형 값이 올바르지 않습니다. 허용되는 값은 'single', 'period', 'recurring'입니다."),
+
+	@ExplainError("예약 유형 값이 누락되었을 때 발생하는 오류입니다.")
+	MISSING_RESERVATION_TYPE(BAD_REQUEST, "ADMIN_RESERVATION_400_4", "예약 유형 값이 올바르지 않습니다. 허용되는 값은 'single', 'period', 'recurring'입니다.");
 
 	private final Integer status;
 	private final String code;
