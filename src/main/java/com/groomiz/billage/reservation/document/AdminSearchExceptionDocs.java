38f3d1658248a48d9ad5c138b498c18770c00d05
orieasy1 <easy1nhard2@gmail.com>
2024-10-04 16:15:59 +0900

    Refactor: seperate Search and status exception docs

--- src/main/java/com/groomiz/billage/reservation/document/AdminSearchExceptionDocs.java
+++ src/main/java/com/groomiz/billage/reservation/document/AdminSearchExceptionDocs.java
@@ -12,12 +12,6 @@ import com.groomiz.billage.reservation.exception.ReservationException;
 @ExceptionDoc
 public class AdminSearchExceptionDocs implements SwaggerExampleExceptions {
 
-	@ExplainError("status 값이 유효하지 않을 때 발생하는 오류입니다.")
-	public GlobalCodeException 잘못된_상태_값 = new AdminReservationException(AdminReservationErrorCode.INVALID_STATUS_FORMAT);
-
-	@ExplainError("status 파라미터가 누락되었을 때 발생하는 오류입니다.")
-	public GlobalCodeException 상태_파라미터_누락 = new AdminReservationException(AdminReservationErrorCode.MISSING_STATUS_PARAMETER);
-
 	@ExplainError("예약 아이디가 존재하지 않는 경우 발생하는 오류입니다.")
 	public GlobalCodeException 존재하지_않는_예약 = new ReservationException(ReservationErrorCode.RESERVATION_NOT_FOUND);
 
