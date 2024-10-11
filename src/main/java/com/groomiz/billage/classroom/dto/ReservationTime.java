a6cf5d4689ac9648f474f2c4275e8350f02cc74f
chaeyoungeee <chaeinge@gmail.com>
2024-10-12 01:02:36 +0900

    SCRUM-26 Feat: 학생 강의실 예약 기능 추가

--- src/main/java/com/groomiz/billage/classroom/dto/ReservationTime.java
+++ src/main/java/com/groomiz/billage/classroom/dto/ReservationTime.java
@@ -2,8 +2,16 @@ package com.groomiz.billage.classroom.dto;
 
 import java.time.LocalTime;
 
+import lombok.Getter;
+
+@Getter
 public class ReservationTime {
 
 	private LocalTime startTime;
 	private LocalTime endTime;
+
+	public ReservationTime(LocalTime startTime, LocalTime endTime) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
 }
