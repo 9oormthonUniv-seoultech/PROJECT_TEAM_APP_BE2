d0b0ddf0b1a1b745f1db5cfa9a921bc751f7e697
chaeyoungeee <chaeinge@gmail.com>
2024-10-19 14:09:37 +0900

    SCRUM-30 Feat: 학생 강의실 상세 정보 조회 기능 추가
    controller에 강의실 id query parameter -> path parameter 변경
    대기/승인 예약만 반환
    시간 jsonformat HH:mm로 세팅

--- src/main/java/com/groomiz/billage/classroom/dto/ReservationTime.java
+++ src/main/java/com/groomiz/billage/classroom/dto/ReservationTime.java
@@ -2,12 +2,16 @@ package com.groomiz.billage.classroom.dto;
 
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 
 @Getter
 public class ReservationTime {
 
+	@JsonFormat(pattern = "HH:mm")
 	private LocalTime startTime;
+	@JsonFormat(pattern = "HH:mm")
 	private LocalTime endTime;
 
 	public ReservationTime(LocalTime startTime, LocalTime endTime) {
