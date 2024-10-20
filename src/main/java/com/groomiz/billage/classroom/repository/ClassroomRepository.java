2a6a5f94ca0931980d6f7064363c1c7e963c2d64
changha <mentenseoul@gmail.com>
2024-10-20 11:57:21 +0900

    SCRUM-32 Feat : Classroom 전체 조회 API
    강의실 전체 조회 API 작성하였습니다.

--- src/main/java/com/groomiz/billage/classroom/repository/ClassroomRepository.java
+++ src/main/java/com/groomiz/billage/classroom/repository/ClassroomRepository.java
@@ -1,8 +1,12 @@
 package com.groomiz.billage.classroom.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.groomiz.billage.classroom.entity.Classroom;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+	Optional<List<Classroom>> findByBuildingIdAndFloorAndCapacityGreaterThanEqual(Long buildingId, Long floor, Integer capacity);
 }
