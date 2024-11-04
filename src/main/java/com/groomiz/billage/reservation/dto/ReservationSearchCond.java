a1efac21f5a4b9603c342d16d93cd7c166e5f150
chaeyoungeee <chaeinge@gmail.com>
2024-11-05 00:19:39 +0900

    Feat: 학생 예약 조회 과거/현재+미래 필터링 추가 및 페이징 기능 추가

--- src/main/java/com/groomiz/billage/reservation/dto/ReservationSearchCond.java
+++ src/main/java/com/groomiz/billage/reservation/dto/ReservationSearchCond.java
@@ -1,13 +1,9 @@
 package com.groomiz.billage.reservation.dto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.groomiz.billage.global.dto.PageRequestDto;
 import com.groomiz.billage.member.entity.Member;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,12 +11,9 @@ import lombok.NoArgsConstructor;
 @Getter
 public class ReservationSearchCond extends PageRequestDto {
 
-	private Member admin;
-	private List<Long> buildingIds = new ArrayList<>();
+	private Member requester;
 
-	@Builder
-	public ReservationSearchCond(Member admin, List<Long> buildingIds) {
-		this.admin = admin;
-		this.buildingIds = buildingIds;
+	public ReservationSearchCond(Member requester) {
+		this.requester = requester;
 	}
 }
